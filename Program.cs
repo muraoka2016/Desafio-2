c55933cee64798787998d4f40800036785c5ee47
muraoka2016 <felipemuraoka2016@outlook.com>
2024-07-12 17:52:16 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+1- Criar uma variável chamada notaMedia e atribua um valor inteiro a ela. Caso seu valor seja maior ou igual a 5, escreva na tela "Nota suficiente para aprovação".
+2- Criar uma lista de linguagens de programação, com as linguagens C#, Java e JavaScript.
+3- Exibir o valor "C#" no console, utilizando a lista criada no exercício anterior.
+4- Elabore uma lista de 10 elementos, peça ao usuário que digite um número de 1 a 10 e retorne o elemento da lista correspondente. */
+
+/*double a = 9;
+double b = 0;
+double media = (a + b) / 2;
+
+if (media >= 5)
+{
+    Console.WriteLine("Aprovado");
+}
+else
+{
+    Console.WriteLine("Reprovado");
+}*/
+
+/*List<string> programLanguages = new List<string> { "C#", "Java", "Javascript" };
+
+Console.WriteLine(programLanguages[0]);*/
+
+/*List<int> numbers = new List<int> { 1, 5, 6, 10, 1000, 500, 400, 300, 950, 8, 74 };
+Console.Write("Digite um número de 1 a 10: ");
+string input = Console.ReadLine()!;
+int convertInput = int.Parse(input);
+
+switch (convertInput)
+{
+    case 0:
+        Console.WriteLine(numbers[0]);
+        break;
+    case 1:
+        Console.WriteLine(numbers[1]);
+        break;
+    case 2:
+        Console.WriteLine(numbers[2]);
+        break;
+    case 3:
+        Console.WriteLine(numbers[3]);
+        break;
+    case 4:
+        Console.WriteLine(numbers[4]);
+        break;
+    case 5:
+        Console.WriteLine(numbers[5]);
+        break;
+    case 6:
+        Console.WriteLine(numbers[6]);
+        break;
+    case 7:
+        Console.WriteLine(numbers[7]);
+        break;
+    case 8:
+        Console.WriteLine(numbers[8]);
+        break;
+    case 9:
+        Console.WriteLine(numbers[9]);
+        break;
+    case 10:
+        Console.WriteLine(numbers[10]);
+        break;
+    default:
+        Console.WriteLine("Opção inválida");
+        break;
+}*/
